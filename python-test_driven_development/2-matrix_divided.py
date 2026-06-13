f822e48fbdbdd91b9669691aced2c052b906e31c
Alanoud-Aloraydi <alanoudkh16@gmail.com>
2026-06-13 20:56:49 +0300

    Fix: W293 blank line contains whitespace

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -27,7 +27,7 @@ def matrix_divided(matrix, div):
 
     if type(div) not in (int, float):
         raise TypeError("div must be a number")
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
