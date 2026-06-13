a668db279e6ca81ebfc69f7b4607cb63943eb4de
Alanoud-Aloraydi <alanoudkh16@gmail.com>
2026-06-13 21:11:32 +0300

    Fix: E501 line too long in docstring

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,7 +8,8 @@ that prints a text with 2 new lines after each '.', '?', and ':'.
 def text_indentation(text):
     """
     Prints text with 2 new lines after '.', '?', and ':'.
-    There should be no space at the beginning or at the end of each printed line.
+    There should be no space at the beginning or at the end
+    of each printed line.
 
     Args:
         text: The text string to be printed.
