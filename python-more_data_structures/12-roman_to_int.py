adf9bf05c3bcd08f13dfb2e7144b19466264ecfc
Alanoud-Aloraydi <alanoudkh16@gmail.com>
2026-06-13 05:07:49 +0300

    fix: pycodestyle W293 whitespace on blank lines

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,17 +7,15 @@ def roman_to_int(roman_string):
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
-    
+
     total = 0
     length = len(roman_string)
-    
+
     for i in range(length):
         val = roman_dict.get(roman_string[i], 0)
-        # إذا كان الرقم الحالي أصغر من الرقم الذي يليه، نقوم بطرحه
         if i + 1 < length and roman_dict.get(roman_string[i + 1], 0) > val:
             total -= val
         else:
-            # غير ذلك نقوم بجمعه
             total += val
-            
+
     return total
