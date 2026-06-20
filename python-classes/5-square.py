94eea09fcde9c43e4a9a050fc3106123fe5a6d7c
Alanoud-Aloraydi <alanoudkh16@gmail.com>
2026-06-20 17:54:41 +0300

    Task 5: Printing a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Module that defines a square."""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size=0):
+        """Initialize the square.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter to retrieve the size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter to set the size with validation.
+
+        Args:
+            value (int): The new size.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
