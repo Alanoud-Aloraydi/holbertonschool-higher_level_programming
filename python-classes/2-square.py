84298801fbca3c584cad2072b405f598d262b9c8
Alanoud-Aloraydi <alanoudkh16@gmail.com>
2026-06-20 17:51:04 +0300

    Fix: Remove hidden spaces and fix pycodestyle

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""
-This module defines a class Square.
-"""
+"""This module defines a class Square."""
 
 
 class Square:
-    """
-    A class that defines a square.
-    """
+    """A class that defines a square."""
+
     def __init__(self, size=0):
-        """
-        Initializes a new Square with type and value validation.
+        """Initializes a new Square.
 
         Args:
-            size (int): The size of the new square. Defaults to 0.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
+            size (int): The size of the square. Defaults to 0.
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        
         self.__size = size
