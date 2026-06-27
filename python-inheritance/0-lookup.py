6e106fd457833c0c6cf578be432c40523b679e58
Alanoud-Aloraydi <alanoudkh16@gmail.com>
2026-06-27 16:25:57 +0300

    Fix: W293 blank line contains whitespace in Task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
-"""
-This module provides a single function `lookup`
-that returns the list of available attributes
-and methods of an object.
-"""
+"""Module that provides a function to inspect an object."""
 
 
 def lookup(obj):
-    """
-    Returns the list of available attributes and methods of an object.
-    
-    Args:
-        obj: The object to inspect.
-        
-    Returns:
-        A list object containing the attributes and methods.
-    """
+    """Returns the list of available attributes and methods of an object."""
     return dir(obj)
