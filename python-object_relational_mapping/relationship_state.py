762b84bef34a1478fcf35de7588f7b107bb9bcf3
Alanoud-Aloraydi <alanoudkh16@gmail.com>
2026-07-25 07:31:30 +0300

    feat(task-15): add SQLAlchemy relationships and script to create linked objects

---
+++ python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Contains the class definition of a State with relationship to City"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+class State(Base):
+    """State class with relationship to cities"""
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
