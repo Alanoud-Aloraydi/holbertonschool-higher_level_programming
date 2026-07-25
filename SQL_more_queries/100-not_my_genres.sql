c420811e0bd70c84d96d252ba693605522096cbd
Alanoud-Aloraydi <alanoudkh16@gmail.com>
2026-07-25 07:16:26 +0300

    fix(task-17): resolve SQL syntax error caused by hidden spacing characters

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -2,9 +2,9 @@
 SELECT name
 FROM tv_genres
 WHERE id NOT IN (
-    SELECT genre_id
-    FROM tv_show_genres
-    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-    WHERE tv_shows.title = 'Dexter'
+SELECT genre_id
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = 'Dexter'
 )
 ORDER BY name ASC;
