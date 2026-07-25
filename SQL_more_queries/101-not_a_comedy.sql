e38d25c46b2243757a4fe0422df810bbbf54ddb8
Alanoud-Aloraydi <alanoudkh16@gmail.com>
2026-07-25 07:16:33 +0300

    fix(task-18): resolve SQL syntax error caused by hidden spacing characters

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -2,9 +2,9 @@
 SELECT title
 FROM tv_shows
 WHERE id NOT IN (
-    SELECT show_id
-    FROM tv_show_genres
-    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-    WHERE tv_genres.name = 'Comedy'
+SELECT show_id
+FROM tv_show_genres
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = 'Comedy'
 )
 ORDER BY title ASC;
