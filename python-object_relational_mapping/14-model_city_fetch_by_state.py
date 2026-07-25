2acc580fcd8bbd6e67b13bf886d5045f9a1b1545
Alanoud-Aloraydi <alanoudkh16@gmail.com>
2026-07-25 07:38:45 +0300

    fix(task-14): strictly enforce PEP8 line limits and implicit joins

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-"""Prints all City objects from the database hbtn_0e_14_usa"""
+"""
+Prints all City objects from the database hbtn_0e_14_usa
+"""
 import sys
-from model_state import Base, State
-from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    # كسر السطر لتجنب تجاوز 79 حرفاً (PEP8)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
-    # استخدام filter بدلاً من join الصريح لتجنب أخطاء غياب الـ relationship
-    query = session.query(State, City).filter(State.id == City.state_id)\
-                                      .order_by(City.id).all()
+
+    # كسر سطر الاستعلام الطويل ليتوافق مع PEP8
+    query = session.query(State, City).filter(
+        State.id == City.state_id
+    ).order_by(City.id).all()
+
     for state, city in query:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
+
     session.close()
